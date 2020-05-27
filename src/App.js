c8ad56e0cb7ce16bea5ab9d476f734577e6a39a4
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-05-27 21:33:07 +0530

    particles added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLink from './Components/ImageLink/ImageLink';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 import 'tachyons';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles"
+          params={particlesOptions}
+
+        />
+
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLink />
+
+        {/*
       
-      <Logo />
-      <ImageLink />
-      {/*
-      <Count / >
       
         <ImageRecognition />
       */}
 
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
